3b9890dbca28fb6ed59155d3d5fa60fb82dc09b6
rabby6530 <you@example.com>
2023-08-26 19:43:31 +0600

    my portfolio server start

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`My Portfolio Website listening on port ${port}`)
+})
