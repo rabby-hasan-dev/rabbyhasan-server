2868e52e54a8cd6048735bde9a60a112f59028dc
rabby6530 <you@example.com>
2023-08-26 20:02:03 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@cluster0.p6rgmv3.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
@@ -11,5 +46,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
+
     console.log(`My Portfolio Website listening on port ${port}`)
 })
